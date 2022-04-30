24eaf4f94227ad17382e4d8e6ee388734e77f3a3
mardsonferreira <mardsonferreira25@gmail.com>
2022-04-29 23:41:17 -0300

    Creating header

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,15 +1,25 @@
 import React from 'react';
+import { StatusBar } from 'react-native';
+import { RFValue } from 'react-native-responsive-fontsize';
 
-import {
-  Container, Title
-} from './styles';
+import Logo from '../../assets/logo.svg';
 
-export function Home(){
-  return (
-    <Container>
-        <Title>
-            Home
-        </Title>
-    </Container>
-  );
+import { Container, Header, HeaderContent, TotalCars } from './styles';
+
+export function Home() {
+    return (
+        <Container>
+            <StatusBar
+                barStyle="light-content"
+                backgroundColor="transparent"
+                translucent
+            />
+            <Header>
+                <HeaderContent>
+                    <Logo width={RFValue(108)} height={RFValue(12)} />
+                    <TotalCars>Total de 12 caros</TotalCars>
+                </HeaderContent>
+            </Header>
+        </Container>
+    );
 }
--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,6 +1,6 @@
 export default {
     colors: {
-        header: '#1B1BIF',
+        header: '#1B1B1F',
 
         background_primary: '#F4F5F6',
         background_secondary: '#FFFFFF',
