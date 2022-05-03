bbb94966c26fd21551d0255a27d9e2e4e6b53960
mardsonferreira <mardsonferreira25@gmail.com>
2022-05-03 20:58:58 -0300

    Creating slider component

--- src/screens/CarDetails/index.tsx
+++ src/screens/CarDetails/index.tsx
@@ -1,16 +1,23 @@
 import React from 'react';
 import { BackButton } from '../../components/BackButton';
+import { ImageSlider } from '../../components/ImageSlider';
 
-import {
-  Container, Header
-} from './styles';
+import { Container, Header, CarImages } from './styles';
 
-export function CarDetails(){
-  return (
-    <Container>
-        <Header>
-            <BackButton />
-        </Header>
-    </Container>
-  );
+export function CarDetails() {
+    return (
+        <Container>
+            <Header>
+                <BackButton />
+            </Header>
+
+            <CarImages>
+                <ImageSlider
+                    imagesUrl={[
+                        'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTnPaKOCq-C-wQ_j5LAXajH4_hm0FBHBsAaaA&usqp=CAU',
+                    ]}
+                />
+            </CarImages>
+        </Container>
+    );
 }
