44798690c942839ff4e70344658dc37935e3fc02
mardsonferreira <mardsonferreira25@gmail.com>
2022-05-05 10:22:08 -0300

    Creating scheduling interface

---
+++ src/screens/Scheduling/index.tsx
@@ -0,0 +1,65 @@
+import React from 'react';
+import { StatusBar } from 'react-native';
+import { useTheme } from 'styled-components';
+
+import { BackButton } from '../../components/BackButton';
+import { Button } from '../../components/Button';
+
+import ArrowSvg from '../../assets/arrow.svg';
+
+import {
+    Container,
+    Header,
+    Title,
+    RrentalPeriod,
+    DateInfo,
+    DateTitle,
+    DateValue,
+    Content,
+    Footer,
+} from './styles';
+
+export function Scheduling() {
+    const theme = useTheme();
+
+    return (
+        <Container>
+            <Header>
+                <StatusBar
+                    barStyle="light-content"
+                    translucent
+                    backgroundColor="transparent"
+                />
+                <BackButton color={theme.colors.shape} />
+
+                <Title>
+                    Escolha uma {'\n'}
+                    data de início e {'\n'}
+                    fim do aluguel
+                </Title>
+
+                <RrentalPeriod>
+                    <DateInfo>
+                        <DateTitle>DE</DateTitle>
+                        <DateValue selected> 50/05/2022 </DateValue>
+                    </DateInfo>
+
+                    <ArrowSvg />
+
+                    <DateInfo>
+                        <DateTitle>ATÉ</DateTitle>
+                        <DateValue selected={false} />
+                    </DateInfo>
+                </RrentalPeriod>
+            </Header>
+
+            <Content>
+
+            </Content>
+
+            <Footer>
+                <Button title='Confirmar' onPress={() => {}}/>
+            </Footer>
+        </Container>
+    );
+}
