cac0ec3bd22cd90d0993d5261c4869828ca5345e
mardsonferreira <mardsonferreira25@gmail.com>
2022-05-06 21:29:51 -0300

    Creating scheduling details interface

--- App.tsx
+++ App.tsx
@@ -12,7 +12,7 @@ import {
     Archivo_600SemiBold,
 } from '@expo-google-fonts/archivo';
 
-import { Scheduling } from './src/screens/Scheduling';
+import { SchedulingDetails } from './src/screens/SchedulingDetails';
 import theme from './src/styles/theme';
 
 export default function App(): JSX.Element {
@@ -30,7 +30,7 @@ export default function App(): JSX.Element {
 
     return (
         <ThemeProvider theme={theme}>
-            <Scheduling />
+            <SchedulingDetails />
         </ThemeProvider>
     );
 }
