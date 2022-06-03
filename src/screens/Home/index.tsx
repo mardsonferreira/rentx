e0c0eda38b03213304c823e2d6c120aa9a07c8ff
mardsonferreira <mardsonferreira25@gmail.com>
2022-06-02 23:58:35 -0300

    handle users gesture

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,9 +1,20 @@
 import React, { useEffect, useState } from 'react';
 import { useNavigation } from '@react-navigation/native';
-import { StatusBar } from 'react-native';
+import { StatusBar, StyleSheet } from 'react-native';
 import { RFValue } from 'react-native-responsive-fontsize';
 import { Ionicons } from '@expo/vector-icons';
 import { useTheme } from 'styled-components';
+
+import { RectButton, PanGestureHandler } from 'react-native-gesture-handler';
+
+import Animated, {
+    useSharedValue,
+    useAnimatedStyle,
+    useAnimatedGestureHandler,
+    withSpring,
+} from 'react-native-reanimated';
+
+const ButtonAnimated = Animated.createAnimatedComponent(RectButton);
 
 import Logo from '../../assets/logo.svg';
 
@@ -13,14 +24,7 @@ import { CarDTO } from '../../dtos/CarDTO';
 import { Car } from '../../components/Car';
 import { Load } from '../../components/Load';
 
-import {
-    Container,
-    Header,
-    HeaderContent,
-    TotalCars,
-    CarList,
-    MyCarsButton,
-} from './styles';
+import { Container, Header, HeaderContent, TotalCars, CarList } from './styles';
 
 interface ICar {
     id: string;
@@ -38,6 +42,32 @@ export function Home() {
     const [loading, setLoading] = useState(true);
     const navigation = useNavigation();
     const theme = useTheme();
+    const positionY = useSharedValue(0);
+    const positionX = useSharedValue(0);
+
+    const myCarsButtonStyle = useAnimatedStyle(() => {
+        return {
+            transform: [
+                { translateX: positionX.value },
+                { translateY: positionY.value },
+            ],
+        };
+    });
+
+    const onGestureEvent = useAnimatedGestureHandler({
+        onStart(_, ctx: any) {
+            ctx.positionX = positionX.value;
+            ctx.positionY = positionY.value;
+        },
+        onActive(event, ctx: any) {
+            positionX.value = ctx.positionX + event.translationX;
+            positionY.value = ctx.positionY + event.translationY;
+        },
+        onEnd() {
+            positionX.value = withSpring(0);
+            positionY.value = withSpring(0);
+        },
+    });
 
     function handleCarDetails(car: CarDTO) {
         navigation.navigate('CarDetails', { car });
@@ -90,14 +120,38 @@ export function Home() {
                     )}
                 />
             )}
-
-            <MyCarsButton onPress={handleOpenMyCars}>
-                <Ionicons
-                    name="ios-car-sport"
-                    size={32}
-                    color={theme.colors.shape}
-                />
-            </MyCarsButton>
+            <PanGestureHandler onGestureEvent={onGestureEvent}>
+                <Animated.View
+                    style={[
+                        myCarsButtonStyle,
+                        { position: 'absolute', bottom: 13, right: 22 },
+                    ]}
+                >
+                    <ButtonAnimated
+                        style={[
+                            styles.button,
+                            { backgroundColor: theme.colors.main },
+                        ]}
+                        onPress={handleOpenMyCars}
+                    >
+                        <Ionicons
+                            name="ios-car-sport"
+                            size={32}
+                            color={theme.colors.shape}
+                        />
+                    </ButtonAnimated>
+                </Animated.View>
+            </PanGestureHandler>
         </Container>
     );
 }
+
+const styles = StyleSheet.create({
+    button: {
+        width: 60,
+        height: 60,
+        borderRadius: 30,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+});
--- src/screens/Home/styles.ts
+++ src/screens/Home/styles.ts
@@ -1,7 +1,6 @@
 import { RFValue } from 'react-native-responsive-fontsize';
 import styled from 'styled-components/native';
 import { FlatList, FlatListProps } from 'react-native';
-import { RectButton } from 'react-native-gesture-handler';
 
 import { CarDTO } from '../../dtos/CarDTO';
 
@@ -42,19 +41,3 @@ export const CarList = styled(
     },
     showVerticalScrollIndicator: false,
 })``;
-
-export const MyCarsButton = styled(RectButton)`
-    width: 60px;
-    height: 60px;
-
-    border-radius: 30px;
-
-    justify-content: center;
-    align-items: center;
-
-    background-color: ${({ theme }) => theme.colors.main};
-
-    position: absolute;
-    bottom: 13px;
-    right: 22px;
-`;
